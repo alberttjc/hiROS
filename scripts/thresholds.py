ed9d269110cf2421c5ea21b21ee4959e65617837
Albert Tan <albert.jctan@gmail.com>
2021-05-30 18:09:29 +1000

    feat: Deleted unused comments

--- scripts/thresholds.py
+++ scripts/thresholds.py
@@ -1,33 +1,3 @@
-"""
-LAB2INT = {
-    "Pause": 0,
-    "Move to the left": 1,
-    "Undo: Rotate arm anti-clockwise": 2, 
-    "No: Head shake": 3, 
-    "Thumbs down": 4, 
-    "I will follow you": 5, 
-    "Do Nothing": 6, 
-    "Watch out": 7, 
-    "Slow down": 8, 
-    "Thumbs up": 9, 
-    "Pointing: Area": 10, 
-    "Resume": 11, 
-    "Repeat: Rotate arm clockwise": 12, 
-    "Follow me": 13, 
-    "Yes: Head nod": 14, 
-    "Come forward": 15, 
-    "Move to the right": 16, 
-    "Move backward": 17, 
-    "Watch me": 18, 
-    "Doing something else": 19, 
-    "Handwave": 20, 
-    "Start": 21, 
-    "Pointing: Object": 22, 
-    "Speed up": 23, 
-    "Stop": 24
-}
-"""
-
 LAB2INT = {
   "Come forward": 0,
   "Do Nothing": 1,
@@ -47,36 +17,6 @@ LAB2INT = {
   "Undo: Rotate arm anti-clockwise": 15,
   "Watch out": 16
 }
-
-"""
-
-
-GESTURE_THRESHOLDS = {
-    "Pause": 0.4,
-    "Move to the left": 0.4,
-    "Undo: Rotate arm anti-clockwise": 0.2, 
-    "No: Head shake": 0.9, 
-    "Thumbs down": 0.3, 
-    "I will follow you": 0.5, 
-    "Watch out": 0.3, 
-    "Slow down": 0.7, 
-    "Thumbs up": 0.3, 
-    "Pointing: Area": 0.4, 
-    "Resume": 0.5, 
-    "Repeat: Rotate arm clockwise": 0.3, 
-    "Follow me": 0.5, 
-    "Yes: Head nod": 0.7, 
-    "Come forward": 0.6, 
-    "Move to the right": 0.4, 
-    "Move backward": 0.7, 
-    "Watch me": 0.5, 
-    "Handwave": 0.7, 
-    "Start": 0.5, 
-    "Pointing: Object": 0.5, 
-    "Speed up": 0.2, 
-    "Stop": 0.9
-}
-"""
 
 GESTURE_THRESHOLDS = {
     "Pause": 0.6,
